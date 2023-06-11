2b67d455d0938b927c9487a0d3d33df9a6deaa32
tex008 <texera009@gmail.com>
2023-06-11 16:11:51 -0300

    finish dark mode toogle

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
 window.onload = () => {
   // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-  if (
-    localStorage.theme === "dark" ||
-    (!("theme" in localStorage) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
+  const checkbox = document.querySelector('input[type="checkbox"]');
+
+  if (localStorage.theme === "light") checkbox.checked = true;
+
+  const soundClick = new Audio("./sounds/click.wav");
 
   const startLightMode = () => {
     document.documentElement.classList.remove("dark");
     localStorage.theme = "light";
+    soundClick.play();
   };
 
   const startDarkMode = () => {
     document.documentElement.classList.add("dark");
     localStorage.theme = "dark";
+    soundClick.play();
   };
 
-  const checkbox = document.querySelector('input[type="checkbox"]');
   checkbox.addEventListener("change", (event) =>
     event.target.checked ? startLightMode() : startDarkMode()
   );
-
-  // // Whenever the user explicitly chooses light mode
-  // localStorage.theme = "light";
-
-  // // Whenever the user explicitly chooses dark mode
-  // localStorage.theme = "dark";
-
-  // // Whenever the user explicitly chooses to respect the OS preference
-  // localStorage.removeItem("theme");
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,18 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        moon: "moonGrow 0.2s linear 0.5s",
+        moon: "moonGrow 0.2s linear 0.2s",
+        sun: "sunGrow 0.2s linear 0.2s",
       },
       keyframes: {
         moonGrow: {
+          "0%": { backgroundSize: "24px 24px" },
+          "50%": { backgroundSize: "20px 20px" },
+          "100%": { backgroundSize: "24px 24px" },
+        },
+        sunGrow: {
           "0%": { backgroundSize: "30px 30px" },
-          "50%": { backgroundSize: "26px 26px" },
+          "50%": { backgroundSize: "24px 24px" },
           "100%": { backgroundSize: "30px 30px" },
         },
       },
