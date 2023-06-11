4954bc7af659bbb68570e20dd5e8c355dac08c8a
tex008 <texera009@gmail.com>
2023-06-11 12:40:47 -0300

    sets the dark thme transition to light theme and vice-versa

---
+++ src/index.js
@@ -0,0 +1,36 @@
+window.onload = () => {
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
+  const startLightMode = () => {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+  };
+
+  const startDarkMode = () => {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+  };
+
+  const checkbox = document.querySelector('input[type="checkbox"]');
+  checkbox.addEventListener("change", (event) =>
+    event.target.checked ? startLightMode() : startDarkMode()
+  );
+
+  // // Whenever the user explicitly chooses light mode
+  // localStorage.theme = "light";
+
+  // // Whenever the user explicitly chooses dark mode
+  // localStorage.theme = "dark";
+
+  // // Whenever the user explicitly chooses to respect the OS preference
+  // localStorage.removeItem("theme");
+};
