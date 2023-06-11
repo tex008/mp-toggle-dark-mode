88df9aee84626c04a0b3c1166bab9d5d5084e69a
tex008 <texera009@gmail.com>
2023-06-11 15:47:26 -0300

    build the animation in the toogle icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   darkMode: "class",
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        moon: "moonGrow 0.2s linear 0.5s",
+      },
+      keyframes: {
+        moonGrow: {
+          "0%": { backgroundSize: "30px 30px" },
+          "50%": { backgroundSize: "26px 26px" },
+          "100%": { backgroundSize: "30px 30px" },
+        },
+      },
+    },
   },
   plugins: [],
 };
